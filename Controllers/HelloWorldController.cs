421eb3941dd3eccea537fc05c398d7496665909a
msghauri3 <msghauri@gmail.com>
2025-11-29 14:29:27 +0500

    HelloWorld ...

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBilling_Lahore_ReactCore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- obj/Debug/net8.0/WebBilling_Lahore_ReactCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebBilling_Lahore_ReactCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBilling_Lahore_ReactCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c53ae9d63b532c13bd0e24e465ded8d50bd57b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+417a95dc6fbd05b582fa59bf53b969961ff9896f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBilling_Lahore_ReactCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBilling_Lahore_ReactCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
